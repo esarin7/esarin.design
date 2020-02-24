3eb370b97d820c143a4295a18d64718ee11d3846
Elena <sosalerin_elena@outlook.com>
2020-02-24 15:21:42 -0700

    Chanegd from JS to CSS the animation of my headline characters.

--- script.js
+++ script.js
@@ -1,44 +1,85 @@
 //CODE FOR TEXT COLOUR AND DYNAMICS
+
 "use strict";
+
+// randomNumber function
+
+const randomNumber = max => Math.floor(Math.random() * max);
+
+// applyAnimationClass function
+
+const applyAnimationClass = () => {
+  const animateOptions = [
+    "animateGreen",
+
+    "animateDarkBlue",
+
+    "animatePink",
+
+    "animateYellow"
+  ];
+
+  const randomIndex = randomNumber(animateOptions.length);
+  return animateOptions[randomIndex];
+};
+
 function _toArray(arr) {
   return Array.isArray(arr) ? arr : Array.from(arr);
 }
+
 function makeSpans(selector) {
   var _document$querySelect = document.querySelectorAll(selector);
+
   var _document$querySelect2 = _toArray(_document$querySelect);
+
   var elements = _document$querySelect2.slice(0);
+
   return elements.map(function(element) {
     var text = element.innerText.split("");
+
     var spans = text
+
       .map(function(letter) {
-        return "<span>" + letter + "</span>";
+        return `<span class='${applyAnimationClass()}'>${letter}</span>`;
       })
+
       .join("");
+
     return (element.innerHTML = spans);
   });
 }
+
 // usage with single selector
+
 makeSpans("h2, h3");
 
 //CODE FOR IMAGE SLIDER
 
 var imageArray = [
   "./assets/guanajuato_gto.jpg",
+
   "./assets/filandia_colombia.jpg",
+
   "./assets/maputo_mozambique.jpg",
+
   "./assets/medellin_colombia.jpg",
+
   "./assets/df_mexico.jpg",
+
   "./assets/bogota_colombia.jpg"
 ];
 
 //Functions
 
 var myImage = document.getElementById("fullimage");
+
 var imageIndex = 0;
 
 setInterval(function() {
   myImage.setAttribute("src", imageArray[imageIndex]);
+
   imageIndex++;
+
   if (imageIndex > 5) {
     imageIndex = 0;
   }
@@ -46,6 +87,7 @@ setInterval(function() {
 
 function changeImage() {
   myImage.setAttribute("src", imageArray[imageIndex]);
+
   imageIndex++;
   if (imageIndex > 5) {
     imageIndex = 0;
@@ -54,9 +96,11 @@ function changeImage() {
 
 function changeImageBack() {
   myImage.setAttribute("src", imageArray[imageIndex]);
+
   imageIndex--;
   if (imageIndex < 0) {
     imageIndex = 5;
   }
 }
+
 document.onload = changeImage();
